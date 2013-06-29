f85f74762c3cf3b698fbc7242df83af924f3c5f6
sancome <sancome@163.com>
2013-06-29 15:48:44 +0800

    modified reference XPEnology project

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -1614,16 +1614,17 @@ static void compat_ioctl_error(struct file *filp, unsigned int fd,
 			fn = "?";
 	}
 
-	 sprintf(buf,"'%c'", (cmd>>_IOC_TYPESHIFT) & _IOC_TYPEMASK);
+	sprintf(buf,"'%c'", (cmd>>_IOC_TYPESHIFT) & _IOC_TYPEMASK);
 	if (!isprint(buf[1]))
 		sprintf(buf, "%02x", buf[1]);
+	#ifndef XPENOLOGY
 	compat_printk("ioctl32(%s:%d): Unknown cmd fd(%d) "
 			"cmd(%08x){t:%s;sz:%u} arg(%08x) on %s\n",
 			current->comm, current->pid,
 			(int)fd, (unsigned int)cmd, buf,
 			(cmd >> _IOC_SIZESHIFT) & _IOC_SIZEMASK,
 			(unsigned int)arg, fn);
-
+	#endif
 	if (path)
 		free_page((unsigned long)path);
 }
